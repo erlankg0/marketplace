cbecd9fd0a59ad6dda0ec586f663eb43cdedc0f0
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-06-12 20:18:54 +0300

    git commit -m "T1-73 <Создание модального окна которая принимает любые компоненты, логика октрытья и закрытья, картики испрвления карточек товаров, создание компонента продовца>"

--- src/components/cardModal/UI/cardModal.tsx
+++ src/components/cardModal/UI/cardModal.tsx
@@ -1,24 +1,53 @@
 import styles from "./cardModal.module.scss";
 import Seller from "@components/seller/UI/seller.tsx";
-
+import sliderMain from "@assets/images/sliderMain.jpg";
+import nitka from "@assets/images/nitki.jpg";
 
 const CardModal = () => {
     return (
         <section className={styles.card}>
-            <div>
-                Slide
-            </div>
-            <div>
-                <div>
-                    <p>Маркетплейс/Инвентарь</p>
-                    <h3>Профессиональные спицы для вязания</h3>
-                    <p>1000 сом</p>
+            <div className={styles.card__gallery}>
+                <div className={styles.card__slide}>
+                    <img src={sliderMain} alt={''}/>
                 </div>
-                <div className={'line'}></div>
-                <div>
+                <div className={styles.card__slides}>
+                    <img className={styles.card__slider} src={nitka} alt={'nitka'}/>
+                    <img className={styles.card__slider} src={nitka} alt={'nitka'}/>
+                    <img className={styles.card__slider} src={nitka} alt={'nitka'}/>
+                    <img className={styles.card__slider} src={nitka} alt={'nitka'}/>
+                    <img className={styles.card__slider} src={nitka} alt={'nitka'}/>
+                </div>
+            </div>
+            <div className={styles.card__content}>
+                <div className={styles.card__header}>
+                    <p className={styles.card__bread}>Маркетплейс/Инвентарь</p>
+                    <h3 className={styles.card__title}>Профессиональные спицы для вязания</h3>
+                    <p className={styles.card__price}>1000 сом</p>
+                </div>
+                <div className={styles.card__text}>
+                    <div className={'line'}></div>
                     <div>
                         <Seller/>
                     </div>
+                    <div className={styles.content}>
+                        <div className={styles.content__tags}>
+                            <div className={styles.content__tag}>
+                                Описание
+                            </div>
+                            <div className={styles.content__tag}>
+                                Контакты Авторов
+                            </div>
+                        </div>
+                        <div className={styles.content}>
+                            <text className={styles.content__text}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                            </text>
+                            <p className={styles.content__count}>В наличие: <strong>2</strong></p>
+                        </div>
+                    </div>
+                    <button className={styles.button}>Купить</button>
                 </div>
             </div>
         </section>

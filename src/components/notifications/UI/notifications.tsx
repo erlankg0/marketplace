346026882d783e5bc741d8d929aca3e94aa3127d
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-08-11 00:14:39 +0300

    try working with websocket

--- src/components/notifications/UI/notifications.tsx
+++ src/components/notifications/UI/notifications.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Notification from "@components/notification/UI/notification.tsx";
 import {INotifications} from "@components/notifications/interface.ts";
 
@@ -6,6 +6,37 @@ import eyes from "@assets/icon/eyes.svg";
 import styles from "./notification.module.scss";
 
 const Notifications: React.FC<INotifications> = ({active}) => {
+    const [messages, setMessages] = useState<string[]>([]);
+    const [ws, setWs] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        const websocket = new WebSocket('https://smarttailor.xyz/websocket');
+
+        websocket.onopen = () => {
+            console.log('Connected');
+        }
+
+        websocket.onmessage = (event: MessageEvent) => {
+            console.log('Get new message', event.data);
+            setMessages(prevState => [...prevState, event.data])
+        }
+
+        websocket.onerror = (error) => {
+            console.error(error);
+        }
+        websocket.onclose = () => {
+            console.log('WebSocket соединение закрыто');
+        };
+
+        setWs(websocket);
+
+        // Очистка соединения при размонтировании компонента
+
+        return () => {
+            websocket.close();
+            ws
+        }
+    }, [])
     return (
         <div className={active ? `${styles.notification} ${styles.active}` : styles.notification}>
             <div className={styles.notification__header}>
@@ -13,6 +44,7 @@ const Notifications: React.FC<INotifications> = ({active}) => {
             </div>
             <div className={'line'}></div>
             <div className={styles.notification__content}>
+                {messages.map((mes) => (<p>{mes}</p>))}
                 <Notification/>
                 <Notification/>
                 <Notification/>
